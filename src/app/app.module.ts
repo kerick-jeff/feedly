a518efd8a26434558a08278acfbb19aaab35acc8
k3r1ck <frukerickjeff@gmail.com>
2018-12-19 11:05:43 +0100

    Updated toast-wrapper global style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import * as firebase from 'firebase/app';
 
 import { MyApp } from './app.component';
 import { LoginPageModule } from '../pages/login/login.module';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAF32hT1Gau-u9rst_hd4QqeY2HWC71W3M",
+  authDomain: "feedly-3f063.firebaseapp.com",
+  databaseURL: "https://feedly-3f063.firebaseio.com",
+  projectId: "feedly-3f063",
+  storageBucket: "feedly-3f063.appspot.com",
+  messagingSenderId: "351107395276"
+});
 
 @NgModule({
   declarations: [
