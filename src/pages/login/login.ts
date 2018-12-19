0074697d77dee67e7972cbec4992b11484121f5e
k3r1ck <frukerickjeff@gmail.com>
2018-12-19 09:03:51 +0100

    Modified login page to allow navigation to signup page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -9,8 +9,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class LoginPage {
 
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams
+    public navCtrl: NavController
   ) { }
 
+  goToSignup() {
+    this.navCtrl.push('SignupPage')
+  }
 }
